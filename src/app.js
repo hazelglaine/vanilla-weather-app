c50aa4304a85d52b8f3ac5e41419041ef565ad8a
hazelglaine <hazelglainesales@gmail.com>
2023-06-29 21:27:13 +0800

    Updated icon to show current weather condition

--- src/app.js
+++ src/app.js
@@ -20,22 +20,29 @@ function formatDate(timestamp) {
 
 function displayTemperature(response) {
   let data = response.data;
-  console.log(data);
   let temperatureElement = document.querySelector("#temperatureValue");
-  temperatureElement.innerHTML = Math.round(data.main.temp);
   let cityElement = document.querySelector("#cityName");
-  cityElement.innerHTML = data.name;
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = data.weather[0].description;
   let humidityElement = document.querySelector("#humidityValue");
-  humidityElement.innerHTML = data.main.humidity;
   let windSpeedElement = document.querySelector("#windSpeedValue");
-  windSpeedElement.innerHTML = Math.round(data.wind.speed);
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  console.log(data);
+  temperatureElement.innerHTML = Math.round(data.main.temp);
+  cityElement.innerHTML = data.name;
+  descriptionElement.innerHTML = data.weather[0].description;
+  humidityElement.innerHTML = data.main.humidity;
+  windSpeedElement.innerHTML = Math.round(data.wind.speed);
   dateElement.innerHTML = formatDate(data.dt);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", data.weather[0].description);
 }
 
-let cityName = "Quezon City";
+let cityName = "Manila";
 
 let apiKey = "f088c6a2edca8c46f2604594d2426c54";
 let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
